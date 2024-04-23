1323e2ba862ca5640ea3fa611fade3c5b7a52979
Richa-G22 <richagarg.22@gmail.com>
2024-04-22 21:43:18 -0400

    fixed restauarntImage model

--- backend/db/models/restaurantimage.js
+++ backend/db/models/restaurantimage.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      RestaurantImage.belongsTo( models.Restaurant, { foreignKey: 'RestaurantId' });
+      RestaurantImage.belongsTo( models.Restaurant, { foreignKey: 'restaurantId' });
     }
   }
   RestaurantImage.init({
